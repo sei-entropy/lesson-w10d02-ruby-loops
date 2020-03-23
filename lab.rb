b0bb5286a1ec730e8e4cd334a5309ad752813f34
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-23 17:11:52 +0300

    done with Loops part

---
+++ lab.rb
@@ -0,0 +1,40 @@
+#---------------------Exercise:Loops------------------- 
+
+# 1/ While
+# input = ""
+# puts "You must guess the Magic Words to exit the while loop!"
+# while input != "Magic Words" do
+#     puts "What are the Magic Words?"
+#     input = gets.chomp
+# end
+# puts "You made it out! Congrats!"
+
+
+# 2/ Until
+# input = ""
+# puts "You must guess the Magic Words to exit the while loop!"
+# until input == "Magic Words" do
+#   puts "What are the Magic Words?"
+#   input = gets.chomp
+# end
+# puts "You made it out! Congrats!"
+
+
+# 3/ Loop
+# puts "You must guess the Magic Words to exit the while loop!"
+# loop do
+#   puts "What are the Magic Words?"
+#   input = gets.chomp
+#   break if input == "Magic Words"
+# end
+# puts "You made it out! Congrats!"
+
+
+
+# 4/ .times
+# users = ["Raghad", "Mohammed", "Alabdulwab"]
+# users.length.times do |index|
+#   puts users[index]  
+# end
+
+
