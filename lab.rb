1300e4b5948111ffc2c3e16da03640c09fcc5276
moh1415 <moh14151415@gmail.com>
2020-03-23 14:46:10 +0300

    lab done

---
+++ lab.rb
@@ -0,0 +1,20 @@
+# first_names = [ "Donald", "Daisy", "Daffy" ]
+# first_names.map do |name|
+#     full_names = name+" Duck"
+#     p full_names
+# end
+
+
+
+
+# numbers = [ 1, 3, 9, 11, 100 ]
+# numbers.map do |num|
+#     p num**2
+# end
+fahrenheit_temps = [ -128.6, 0, 32, 140, 212 ]
+# fahrenheit_temps.map {|tem| p (tem-32) * 5/9 }
+# fahrenheit_temps.map do |temp| 
+#     t1 = temp - 32
+#     t2 = 5/9.to_f
+#     p t1 * t2
+# end
