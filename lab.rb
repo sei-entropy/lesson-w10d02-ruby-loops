f1b64a3cd70b82b6376593d39e9aba2c1fd548e0
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-23 17:13:08 +0300

    done with Each part and bonus

--- lab.rb
+++ lab.rb
@@ -38,3 +38,46 @@
 # end
 
 
+#---------------------Exercise:Each------------------- 
+
+# 1/ Say hello to everybody in the below array of names (sample output: Hello Donald!).
+# names = [ "Donald", "Daisy", "Huey", "Duey", "Luey" ]
+
+# names.each do |name|
+#   puts "Hello #{name}!"
+# end
+
+# 2/ Print out the squared values of every number in this numbers array.
+# numbers = [ 1, 3, 9, 11, 100 ]
+
+# numbers.each do |number|
+#   puts "The squared value of #{number} is #{number**2}"
+# end
+
+# 3/ Print out the Celsius values for an array containing Fahrenheit values.
+# fahrenheit_temps = [ -128.6, 0, 32, 140, 212 ]
+
+# fahrenheit_temps.each do |fahrenheit_temp|
+#   puts "The Celsius value of #{fahrenheit_temp} is #{(fahrenheit_temp - 32) * 5 / 9}"
+# end
+
+# 4/ Insert all the values in the artists array into the ninja_turtles array.
+# artists = [ "Leonardo", "Donatello", "Raphael", "Michelangelo" ]
+# ninja_turtles = []
+
+# artists.each do |artist|
+#   ninja_turtles << artist
+#   puts " ADD #{artist} ==> ninja_turtle "
+# end
+# puts "artists-----------------------"
+# puts "artists: #{artists}"
+# puts "ninja_turtle-----------------------"
+# puts "ninja_turtles: #{ninja_turtles}"
+
+
+# BONUS 
+# Print out every possible combination of the below ice cream flavors and toppings.
+# flavors = [ "vanilla", "chocolate", "strawberry", "butter pecan", "cookies and cream", "rainbow" ]
+# toppings = [ "gummi bears", "hot fudge", "butterscotch", "rainbow sprinkles", "chocolate sprinkles" ]
+# puts flavors.product(toppings)
+
